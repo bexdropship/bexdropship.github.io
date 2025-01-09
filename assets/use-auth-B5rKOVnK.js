712bf66475e7f3bb06f855eff41e2b79f65d1266
limitpointinf0 <coneal0523@protonmail.com>
2025-01-09 14:22:41 -0800

    Updates

---
+++ assets/use-auth-B5rKOVnK.js
@@ -0,0 +1,1 @@
+import{C as s,r,F as e}from"./index-CLMA9LPH.js";const a=()=>{const t=s();return r.useEffect(()=>{e.get("jwt")||t.push("/login")},[t]),null};export{a as u};
--- assets/use-auth-L8330Noa.js
+++
@@ -1,1 +0,0 @@
-import{C as s,r,F as e}from"./index-CO2zk-35.js";const a=()=>{const t=s();return r.useEffect(()=>{e.get("jwt")||t.push("/login")},[t]),null};export{a as u};
